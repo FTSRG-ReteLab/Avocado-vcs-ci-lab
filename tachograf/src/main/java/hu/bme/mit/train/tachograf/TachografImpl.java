8f8d629e0494cd5e611e692466b69a3b8b5cc522
Szántó Benedek Dávid <szantobenedek@gmail.com>
2019-02-11 08:01:28 -0800

    Implement tachograf

---
+++ tachograf/src/main/java/hu/bme/mit/train/tachograf/TachografImpl.java
@@ -0,0 +1,8 @@
+package hu.bme.mit.train.tachograf;
+
+import com.google.common.collect.HashBasedTable;
+import com.google.common.collect.Table;
+
+public class TachografImpl {
+    private Table<Integer,Integer,Integer> table = HashBasedTable.create();+
+}
